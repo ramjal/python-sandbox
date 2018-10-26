3b85b63b26f75bd393bf6977283569449d3e1eb0
rjalali <rjalali@westlandinsurance.ca>
2018-10-25 18:01:56 -0700

    finished the Exercise

--- think_python/chapter_4/Exercise 4.12.pie.py
+++ think_python/chapter_4/Exercise 4.12.pie.py
@@ -1,84 +1,53 @@
 import turtle
 import math
 
-bob = turtle.Turtle()
+trtl = turtle.Turtle()
 
 # draws a poliyline
-def polyline(t, step_length, n, step_angle):
+def polyline(t, step_length, n):
+    step_angle = 360/n
     for i in range(n):
         t.fd(step_length)
         t.lt(step_angle)
 
-# draws a sqare
-def square(t, length):    
-    polyline(t, length, 4, 90)
 
-# draws a polygon
-def polygon(t, length, n):
-    # more accurate than 360 / n
-    # when numerator is float the result will also be float
-    angle = 360.0 / n 
-    polyline(t, length, n, angle)
+def polyline2(t, step_length, n):
+    step_angle = 360 / n
+    inside_angle = (180 - step_angle) / 2
+    inside_step = step_length / (2 * (math.sin(math.pi / n)))
+    print(inside_step)
+    print(math.sin(180 / n))
+    for i in range(n):
+        t.fd(step_length)
+        t.lt(step_angle)
+        t.lt(inside_angle)
+        t.fd(inside_step)
+        t.backward(inside_step)
+        t.rt(inside_angle)
 
-# draws a circle with radius of r
-def circle(t, r):
-    arc(t, r, 360)
+trtl.penup()
+trtl.backward(350)
+trtl.pendown()
 
-# draws an arc with radius of r
-def arc(t, r, angle):
-    n = 100 # the larger n the cleaner arc
-    arc_length = 2 * math.pi * r * angle / 360
-    step_length = arc_length / n
-    step_angle = float(angle) / n
-    polyline(t, step_length, n, step_angle)
+trtl.lt(10)
+polyline2(trtl, 100, 5)
 
+trtl.penup()
+trtl.setx(0)
+trtl.sety(0)
+trtl.pendown()
 
-def test(rep, radius):
-    angle = 360 / rep
-    for i in range(rep):
-        for j in range(2):
-            arc(bob, radius, angle)
-            bob.lt(180 - angle)
-        bob.lt(angle)
+trtl.lt(20)
+polyline2(trtl, 100, 6)
+
+trtl.penup()
+trtl.setx(300)
+trtl.sety(0)
+trtl.pendown()
+
+trtl.lt(10)
+polyline2(trtl, 80, 7)
     
-bob.penup()
-bob.backward(300)
-bob.pendown()
-
-test(7, 100)
-
-bob.penup()
-bob.forward(300)
-bob.pendown()
-
-test(20, 200)
-
-    
-"""
-bob.penup()
-bob.forward(300)
-bob.pendown()
-        
-square(bob, 100) # or we can do: square(bob, length=100)
-
-bob.penup()
-bob.backward(200)
-bob.pendown()
-
-polygon(bob, 50, 8)
-
-bob.penup()
-bob.backward(200)
-bob.pendown()
-
-circle(bob, 80)
-
-bob.penup()
-bob.backward(200)
-bob.pendown()
-   
-arc(bob, 80, 270)
-"""
 
 # tells the window to wait for the user to do something
 # turtle.mainloop()
