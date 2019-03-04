55ffe664554633a09c1d9909dad18009d057029c
rjalali <rjalali@westlandinsurance.ca>
2019-03-04 10:56:08 -0800

    Testing graph

---
+++ graph/dfs/dfs-final.py
@@ -0,0 +1,61 @@
+
+class Vertix:
+    def __init__(self, name):
+        self.discovered = False
+        self.name = name
+        self.neighbors = list()
+
+    def add_neighbor(self, v):
+        if v not in self.neighbors:
+            self.neighbors.append(v)
+            self.neighbors.sort()
+
+
+class Graph:
+    def __init__(self, vertices, edgeList):
+        self.vertices = {}
+        for n in vertices:
+            vertix = Vertix(n) #name            
+            for edge in edgeList:
+                if edge[0] == vertix.name:
+                    vertix.add_neighbor(edge[1])
+            self.vertices[n] = vertix
+
+    def print_graph(self):
+        for vertix in self.vertices.values():
+            print(vertix.name, '->', vertix.neighbors)
+        
+    def dfsRecursive(self, name):
+        vertex = self.vertices[name]
+        if not vertex.discovered:
+            vertex.discovered = True
+            print(vertex.name)       
+            for vName in vertex.neighbors:
+                self.dfsRecursive(vName)
+
+    def dfsIterative(self, name):
+        vertex = self.vertices[name]
+        stack = []
+        stack.append(vertex)
+        while stack:
+            vertex = stack.pop()
+            if not vertex.discovered:
+                vertex.discovered = True
+                print(vertex.name)
+                for vName in vertex.neighbors:
+                    stack.append(self.vertices[vName])
+
+
+
+
+vertices = [0, 1, 2, 3, 4, 5, 6]
+edgeList = [(0,1), (0,2), (1,0) , (1,3) , (2,0) , (2,4) , (2,5) , (3,1), (4,2) , (4,6), (5,2), (6,4)]
+g = Graph(vertices, edgeList)
+g.print_graph()
+print("--------- Recursive -----------")
+g.dfsRecursive(0)
+g = Graph(vertices, edgeList)
+print("--------- Iterative -----------")
+g.dfsIterative(0)
+
+
